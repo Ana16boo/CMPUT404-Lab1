a4cfca5a43c961a78b231952840eb164a1290803
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-01-13 20:12:09 -0500

    Version for Part 11

--- versionRequests.py
+++ versionRequests.py
@@ -3,5 +3,5 @@ import requests
 if __name__ == '__main__':
     print(requests.__version__)
 
-    r = requests.get('https://www.google.com')
+    r = requests.get('https://raw.githubusercontent.com/Ana16boo/CMPUT404-Lab1/main/versionRequests.py')
     print(r.text)
